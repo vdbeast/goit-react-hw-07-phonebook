a301b441f8926f55fd7f1d66f50c87ababce7095
vdbeast <122833284+vdbeast@users.noreply.github.com>
2023-09-03 19:29:26 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -23,18 +23,6 @@ const contactsSlice = createSlice({
     name: 'contacts',
     initialState,
     reducers: {
-        // addContact: (state, action) => {
-        //     const existingContact = state.contacts.items.find(contact => contact.name === action.payload.name);
-            
-        //     if (!existingContact) {
-        //         state.contacts.items.push(action.payload);
-        //     } else {
-        //         alert("Contact already exists");
-        //     }
-        // },
-        // deleteContact: (state, action) => {
-        //     state.contacts = state.contacts.items.filter(contacts => contacts.id !== action.payload)
-        // },
         setFilter: (state, action) => {
             state.filter = action.payload
         }
